36a5e67be242b8f54fcaa93112abc3078e7745a6
lorajdavis <username@users.noreply.github.com>
2016-09-06 10:12:10 -0400

    JSONModelValidation

--- backend/spec/model_resource_spec.rb
+++ backend/spec/model_resource_spec.rb
@@ -6,7 +6,7 @@ describe 'Resource model' do
   it "throws an error when no container summary is provided" do
     opts = {:container_summary => nil}
 
-    expect { create_resource(opts) }.to raise_error
+    expect { create_resource(opts) }.to raise_error(JSONModel::ValidationException)
   end
 
 
